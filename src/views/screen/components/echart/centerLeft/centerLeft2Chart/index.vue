a96ec1fb940c27fec5efc0c5ad02020e772a63ba
YYZ <3158139605@qq.com>
2022-08-29 14:22:28 +0800

    地图

--- src/views/screen/components/echart/centerLeft/centerLeft2Chart/index.vue
+++ src/views/screen/components/echart/centerLeft/centerLeft2Chart/index.vue
@@ -5,62 +5,108 @@
 </template>
 
 <script>
-import Chart from './chart.vue';
+import Chart from "./chart.vue";
 export default {
-  data () {
+  data() {
     return {
       cdata: [
         {
           // 名字需要与 “common/map/fujian.js” 地图数据文件里面定义的一一对应，不能是 “福州” 或者 “闽” 之类的缩写
-          name: '福州市',
+          name: "广州市",
           value: 10,
-          elseData:{
+          elseData: {
             // 这里放置地图 tooltip 里想显示的数据
-          }
+          },
         },
         {
-          name: '厦门市',
-          value: 9,
+          name: "韶关市",
+          value: 10,
         },
         {
-          name: '漳州市',
-          value: 8,
+          name: "深圳市",
+          value: 10,
         },
         {
-          name: '泉州市',
-          value: 7,
+          name: "珠海市",
+          value: 10,
         },
         {
-          name: '三明市',
-          value: 6,
+          name: "汕头市",
+          value: 10,
         },
         {
-          name: '莆田市',
-          value: 5,
+          name: "佛山市",
+          value: 10,
         },
         {
-          name: '南平市',
-          value: 4,
+          name: "江门市",
+          value: 10,
         },
         {
-          name: '龙岩市',
-          value: 3,
+          name: "湛江市",
+          value: 10,
         },
         {
-          name: '宁德市',
-          value: 2,
-        }
-      ]
-    }
+          name: "茂名市",
+          value: 10,
+        },
+        {
+          name: "肇庆市",
+          value: 10,
+        },
+        {
+          name: "惠州市",
+          value: 10,
+        },
+        {
+          name: "梅州市",
+          value: 10,
+        },
+        {
+          name: "汕尾市",
+          value: 10,
+        },
+        {
+          name: "河源市",
+          value: 10,
+        },
+        {
+          name: "阳江市",
+          value: 10,
+        },
+        {
+          name: "清远市",
+          value: 10,
+        },
+        {
+          name: "东莞市",
+          value: 10,
+        },
+        {
+          name: "中山市",
+          value: 10,
+        },
+        {
+          name: "潮州市",
+          value: 10,
+        },
+        {
+          name: "揭阳市",
+          value: 10,
+        },
+        {
+          name: "云浮市",
+          value: 10,
+        },
+      ],
+    };
   },
   components: {
     Chart,
   },
-  mounted () {
-  },
-  methods: {
-  }
-}
+  mounted() {},
+  methods: {},
+};
 </script>
 
 <style lang="scss" scoped>
